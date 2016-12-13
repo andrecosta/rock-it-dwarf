8787d18142f4f6acd56133a6dfe2b9642417be0c
Andre Costa <fc.andre.costa@gmail.com>
2016-12-13 02:12:56 +0000

    arrow animation

--- Assets/Scripts/Arrow.cs
+++ Assets/Scripts/Arrow.cs
@@ -8,10 +8,12 @@ public class Arrow : MonoBehaviour {
     private Vector3 _shootDirection;
     private GameController _gc;
     private float despawnTime = 5;
+    private Animator _animator;
 
     // Use this for initialization
     void Start () {
         _gc = GameController.Instance;
+        _animator = GetComponent<Animator>();
         transform.position = new Vector3(Mathf.Round(transform.position.x * 10) / 10, Mathf.Round(transform.position.y * 10) / 10);
         _shootDirection = (new Vector3(targetTile.X - transform.position.x, targetTile.Y - transform.position.y, 0).normalized);
     }
@@ -19,18 +21,19 @@ public class Arrow : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-
         transform.position += _shootDirection * Time.deltaTime * 4;
         Tile tile = GameController.Instance.GetTileAt(transform.position.x, transform.position.y);
         if (tile != null && tile.Type == TileType.Empty)
         {
             _shootDirection = Vector3.zero;
+            _animator.SetFloat("Speed", 0);
             Destroy(gameObject, 5);
         }
 
         if (Vector3.Distance(_gc.player.transform.position, transform.position) < 0.2f)
         {
             _shootDirection = Vector3.zero;
+            _animator.SetFloat("Speed", 0);
             Destroy(gameObject, 5);
             _gc.PlayerDeath();
         }
