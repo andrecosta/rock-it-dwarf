de99664e9efb140c5eb56c04137f445eeef3e955
Andre Costa <fc.andre.costa@gmail.com>
2016-12-28 05:53:45 +0000

    main menu blinking text

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public Text AnyKeyText;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        // "Continue" text blink
+        AnyKeyText.color = Mathf.Sin(Time.unscaledTime * 10) > 0
+            ? new Color(219 / 255f, 211 / 255f, 205 / 255f)
+            : new Color(180 / 255f, 147 / 255f, 122 / 255f);
+    }
+}
